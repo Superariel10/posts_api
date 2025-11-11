04c262f975ea8ad69c01d1bc8e789fbd7212742f
IDIC <alumnos@alumnosuio.ute.edu.ec>
2025-11-11 10:10:07 -0500

    Clase 11/11/2025

---
+++ dist/basics/basics.controller.d.ts
@@ -0,0 +1,10 @@
+import { BasicsService } from './basics.service';
+export declare class BasicsController {
+    private readonly basicsService;
+    constructor(basicsService: BasicsService);
+    getMyFirstGet(): object;
+    getConParametros(parametro: string): object;
+    create(bodyPost: object): object;
+    update(bodyPost: object, id: string): object;
+    delete(id: string): object;
+}
