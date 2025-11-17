6d717587a8f30e9ed6a171f159d9bfebe548bedc
IDIC <alumnos@alumnosuio.ute.edu.ec>
2025-11-17 09:58:40 -0500

    Clase 17-11-2025

--- dist/categories/categories.controller.d.ts
+++ dist/categories/categories.controller.d.ts
@@ -1,2 +1,12 @@
+import { CategoriesService } from './categories.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
 export declare class CategoriesController {
+    private readonly categoriesService;
+    constructor(categoriesService: CategoriesService);
+    create(createCategoryDto: CreateCategoryDto): Promise<import("./categories.entity").Category>;
+    findAll(): Promise<import("./categories.entity").Category[]>;
+    findOne(id: string): Promise<import("./categories.entity").Category | null>;
+    update(id: string, updateCategoryDto: UpdateCategoryDto): Promise<import("./categories.entity").Category | null>;
+    remove(id: string): Promise<import("./categories.entity").Category | null>;
 }
--- dist/posts/posts.service.d.ts
+++ dist/posts/posts.service.d.ts
@@ -1,2 +1,15 @@
+import { Repository } from 'typeorm';
+import { Post } from './post.entity';
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
+import { Category } from '../categories/categories.entity';
 export declare class PostsService {
+    private readonly postRepository;
+    private readonly categoryRepository;
+    constructor(postRepository: Repository<Post>, categoryRepository: Repository<Category>);
+    create(createPostDto: CreatePostDto): Promise<Post>;
+    findAll(): Promise<Post[]>;
+    findOne(id: string): Promise<Post | null>;
+    update(id: string, updatePostDto: UpdatePostDto): Promise<Post>;
+    remove(id: string): Promise<Post>;
 }
